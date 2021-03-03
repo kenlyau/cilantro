e589db8bab98dddb4da8f2ed81ca2657535b1879
kenlyau <newallah@gmail.com>
2021-03-03 21:40:03 +0800

    add esc

---
+++ go.mod
@@ -0,0 +1,9 @@
+module kenlyau.com/cilantro
+
+go 1.13
+
+require (
+	github.com/alibabacloud-go/ecs-20140526 v1.2.0
+	github.com/denverdino/aliyungo v0.0.0-20210222084345-ddfe3452f5e8
+	github.com/spf13/viper v1.7.1
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/denverdino/aliyungo/ecs"
+	"github.com/spf13/viper"
+)
+
+var ACCESS_KEY_ID string
+var ACCESS_KEY_SECRET string
+var INSTANCE_TYPE string
+var IMAGE_ID string
+var SECURITY_GROUP_ID string
+var V_SWITCH_ID string
+var PASSWORD string
+var REGION_ID string
+
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	ACCESS_KEY_ID = viper.GetString("ACCESS_KEY_ID")
+	ACCESS_KEY_SECRET = viper.GetString("ACCESS_KEY_SECRET")
+	INSTANCE_TYPE = viper.GetString("INSTANCE_TYPE")
+	IMAGE_ID = viper.GetString("IMAGE_ID")
+	SECURITY_GROUP_ID = viper.GetString("SECURITY_GROUP_ID")
+	V_SWITCH_ID = viper.GetString("V_SWITCH_ID")
+	PASSWORD = viper.GetString("PASSWORD")
+	REGION_ID = viper.GetString("REGION_ID")
+
+	client := ecs.NewClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+
+	ecsInstance := &ECSInstance{
+		Client:          client,
+		Region:          REGION_ID,
+		InstanceType:    INSTANCE_TYPE,
+		VSwitchId:       V_SWITCH_ID,
+		ImageId:         IMAGE_ID,
+		SecurityGroupId: SECURITY_GROUP_ID,
+		Password:        PASSWORD,
+	}
+
+	err = ecsInstance.Create("1h")
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Printf("create ecs instance success, [%s] \n", ecsInstance.ID)
+	// createInstanceArgs := ecs.CreateInstanceArgs{
+	// 	RegionId:                REGION_ID,
+	// 	InstanceType:            INSTANCE_TYPE,
+	// 	ImageId:                 IMAGE_ID,
+	// 	SecurityGroupId:         SECURITY_GROUP_ID,
+	// 	VSwitchId:               V_SWITCH_ID,
+	// 	Password:                PASSWORD,
+	// 	InstanceChargeType:      "PostPaid",
+	// 	SpotStrategy:            "SpotAsPriceGo",
+	// 	InternetMaxBandwidthOut: 5,
+	// 	InternetChargeType:      "PayByTraffic",
+	// }
+	// runInstanceArgs := &ecs.RunInstanceArgs{
+	// 	CreateInstanceArgs: createInstanceArgs,
+	// 	MaxAmount:          1,
+	// }
+	// ins, err := client.RunInstances(runInstanceArgs)
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
+}
